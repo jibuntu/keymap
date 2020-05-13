d3eca7da881de20ba513e6b952b3e49224676417
jibuntu <i.am.tomotatu@gmail.com>
2020-05-13 23:44:19 +0900

    KeyConverter::{push(), leave()}を作成

--- src/key_converter/mod.rs
+++ src/key_converter/mod.rs
@@ -8,29 +8,59 @@ use self::rules::Rules;
 use self::rules::Key;
 
 
-#[derive(Debug, PartialEq)]
-pub enum KeyState {
-    Push(u16),
-    Repeat(u16),
-    Leave(u16)
-}
-
 pub struct KeyConverter {
-//    keys: HashSet<Key>, // 実際に押されているキーのリスト
-//    vkeys: HashSet<Key>, // 仮想的に押されているキーのリスト
-//    rule: Rules,
+    keys: HashSet<Key>, // 実際に押されているキーのリスト
+    vkeys: HashSet<Key>, // 仮想的に押されているキーのリスト
+    rules: Rules,
 }
 
 impl KeyConverter {
-    /*pub fn with_rule(rule: Rules) -> KeyConverter {
+    pub fn new() -> KeyConverter {
         KeyConverter {
             keys: HashSet::new(),
             vkeys: HashSet::new(),
-            rule
+            rules: Rules::test_new(),
         }
     }
-    pub fn push(&mut self, key: Key) -> Vec<KeyState> { Vec::new() }
-    pub fn leave(&mut self, key: Key) -> Vec<KeyState> { Vec::new() }
-    */
+    
+    /// 前回とのvkeysの差分を元に返り値を返す。
+    /// 返り値は押すキーと離すキー
+    pub fn push(&mut self, k: u16) -> (Vec<u16>, Vec<u16>) {
+        self.keys.insert(Key::Raw(k));
+        let vk = self.rules.filter(&self.keys);
+
+        // vk - vkeys の結果のキーを押す
+        let push = vk.difference(&self.vkeys).map(|k| k.to_u16()).collect();
+
+        // vkeysに入っていて、vkに入っていないキーを離す
+        // vkeys - vk の結果のキーを離す
+        let leave = self.vkeys.difference(&vk).map(|k| k.to_u16()).collect();
+
+        // self.vkeysの値を更新する
+        self.vkeys = vk;
+
+        (push, leave)
+    }
+
+    /// 前回とのvkeysの差分を元に返り値を返す。
+    /// ここではState::Pushとなるキーは返さない。
+    /// 返り値は離すキーのリスト
+    pub fn leave(&mut self, k: u16) -> Vec<u16> { 
+        self.keys.remove(&Key::Raw(k));
+        let vk = self.rules.filter(&self.keys);
+        
+        // vkeysに入っていて、vkに入っていないキーを離す
+        // vkeys - vk の結果のキーを離す
+        let leave = self.vkeys.difference(&vk).map(|k| k.to_u16()).collect();
+
+        // self.vkeysの値を更新する
+        self.vkeys = vk;
+
+        leave
+    }
+
+    pub fn filter_to_string(&mut self) -> String {
+        self.rules.filter_to_string(&self.keys)
+    }
 }
 
